a5f51fbf5e07e2db9339d211da33bf1d2490c545
Mark Snape <mark@novate.co.uk>
2020-05-12 11:24:21 +0100

    alterations to factory and seeding

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,8 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(RolesAndPermissionsSeeder::class);
 
-        factory(App\User::class, 10)->create();
-        factory(App\Foodbank::class, 34)->create();
+        factory(App\Foodbank::class, 20)->create();
         factory(App\Address::class, 20)->create();
         factory(App\Contact::class, 20)->create();
         factory(App\Contactable::class, 30)->create();
