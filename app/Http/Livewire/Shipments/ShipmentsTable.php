6741b58563d75bd2579d5fb4a598df0d10d4b7eb
Mark Snape <mark@novate.co.uk>
2020-08-06 16:23:12 +0100

    Update shipments to include from and to, and make allocations clickable

--- app/Http/Livewire/Shipments/ShipmentsTable.php
+++ app/Http/Livewire/Shipments/ShipmentsTable.php
@@ -16,7 +16,7 @@ class ShipmentsTable extends TableComponent
 
     public function query()
     {
-        return Shipment::with('fromAddress','toAddress','allocations');
+        return Shipment::with('fromAddress.addressable','toAddress.addressable','allocations');
 
     }
 
@@ -24,21 +24,39 @@ class ShipmentsTable extends TableComponent
     {
         return [
             Column::make('ID')->searchable()->sortable(),
-            // Column::make('From','fromAddress.address1'),
-            // Column::make('To','toAddress'),
+            Column::make('From','from_address.addressable.name'),
+            Column::make('To','to_address.addressable.name'),
             Column::make('Allocations'),
             Column::make('Updated At')->searchable()->sortable(),
         ];
     }
 
+    public function thClass($attribute)
+    {
+        dump($attribute);
+        if ($attribute == 'id') return 'w-1/12';
+        if ($attribute == 'from_address.addressable.name') return 'w-3/12 text-left';
+        if ($attribute == 'to_address.addressable.name') return 'w-3/12 text-left';
+        if ($attribute == 'updated_at') return 'w-2/12 text-left';
+ 
+        return null;
+    }
+    
+
     public function tdPresenter($attribute, $value)
     {
         // if ($attribute == 'total') return '£' .  number_format($value / 100, 2);
-        if ($attribute == 'updated_at') return Carbon::parse($value)->format('H:i D d.m.y');
-        if ($attribute == 'allocations') return collect($value)->implode('id', ', ');
+        if ($attribute == 'updated_at') return Carbon::parse($value)->format('H:i d/m/y');
+        if ($attribute == 'allocations') return $this->allocationsList($value);
         return $value;
     }
 
+    public function allocationsList($value)
+    {
+        return collect($value)->pluck('id')->transform(function($id) {
+            return '<a class="hover:underline" href="' . route('allocations.show',$id) . '">' . $id . '</a>';
+        })->implode(', ');
+    }
 
     public function rowClick($id)
     {
