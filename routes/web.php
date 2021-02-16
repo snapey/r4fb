bb1d3cbf946bcdd59d171c564a4305ff2eeb90ef
Mark Snape <mark@novate.co.uk>
2021-02-16 14:40:45 +0000

    typo in shipments permission

--- routes/web.php
+++ routes/web.php
@@ -72,9 +72,9 @@ Route::middleware(['auth', 'nocache'])->group(function () {
     Route::get('prepareOrders', 'PrepareOrdersController@show')->name('prepare-orders');
     Route::get('prepareorders/{allocation}/single', 'PrepareOrdersController@single')->name('prepare.orders.single');
 
-    Route::get('shipment', 'ShipmentController@index')->name('shipment.index')->middleware('permission:Shipment.view');
+    Route::get('shipment', 'ShipmentController@index')->name('shipment.index');//->middleware('permission:Shipments.view');
     Route::get('shipment/multi', 'ShipmentController@multi')->name('shipment.multi');
-    Route::get('shipment/{shipment}', 'ShipmentController@show')->name('shipment.show')->middleware('permission:Shipment.view');;
+    Route::get('shipment/{shipment}', 'ShipmentController@show')->name('shipment.show')->middleware('permission:Shipments.view');;
     Route::get('shipment/{allocation}/create', 'ShipmentController@create')->name('shipment.create');
     Route::post('shipment', 'ShipmentController@store')->name('shipment.store');
     Route::get('shipment/{shipment}/pdf', 'ShipmentController@multipdf')->name('shipment.pdf.multi');
